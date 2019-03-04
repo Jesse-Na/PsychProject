befa943274ed854a87dc440f7189a8faa0788fac
Jason Guo <jasonyzguo@gmail.com>
2019-03-04 17:58:24 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+class Word {
+	constructor(word) {
+		this.word = word;
+		this.x = random(width);
+	}
+
+}
+
+function setup() {
+	createCanvas(displayWidth, displayHeight);
+
+}
+function draw() {
+	ellipse(200, 200, 200, 200);
+}
