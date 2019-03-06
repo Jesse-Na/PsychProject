d4dc0f9987c3d58294c0b42508b6844d647d1140
Jason Guo <jasonyzguo@gmail.com>
2019-03-05 21:34:05 -0500

    create logic of game with Hamlet text

--- Word.js
+++ Word.js
@@ -1,24 +1,27 @@
-class Word {
-	constructor(word) {
-		this.word = word;
-		this.wordSize = 20 + Math.random() * 50;
+function Word(word, displayWidth) {
+	this.word = word.toLowerCase();
+	this.wordSize = 20 + Math.random() * 50;
 
-		this.speedX = 0;
-		this.speedY = 2;
+	this.speedX = 0;
+	this.speedY = 2;
 
-		this.x = 200 + Math.random() * ((new p5).displayWidth - 300);
-		this.y = 0;
+	this.x = 200 + Math.random() * (displayWidth - 300) || 200;
+	this.y = 0;
+
+	this.length = function() {
+		return this.word.length;
 	}
-	get firstLetter() {
+
+	this.firstLetter = function() {
 		return this.word.charAt(0);
 	}
 
-	display() {
+	this.display = function() {
 		textSize(this.wordSize);
 		text(this.word, this.x, this.y);
 	}
 
-	update(keyPressed) {
+	this.update = function(keyPressed) {
 		this.x += this.speedX;
 		this.y += this.speedY;
 		if (keyPressed) this.word = this.word.slice(1);
--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-let fallingWords = [];
+let fallingWords;
 function setup() {
 	createCanvas(displayWidth, displayHeight);
-
+	fallingWords = [];
+	//fallingWords.push(new FallingWord());
 }
 function draw() {
 	background(255);
-	fallingWords.forEach((word) => {
-		word.display();
-	//	word.update();
-});
-addWords(fallingWords);
-}
-function mousePressed() {
-	n.update(true);
-}
+ 	fallingWords.forEach((word) => {
+ 		word.display();
+ 		word.update();
+  });
+	if (fallingWords[0]) {
+	 if (fallingWords[0].y > displayHeight) {fallingWords.shift(); console.log('delete');};
+	}
+	if (frameCount % 100 == 0) addWords();
+	}
+ function addWords() {
+	 fallingWords.push(new Word(randomWord(), displayWidth));
+ }
+ function randomWord() {
+	 return hamletText[Math.floor(Math.random() * hamletText.length)].trim();
+ }
 function keyPressed() {
-	console.log(n.firstLetter.charCodeAt(0));
-	if (n.firstLetter.charCodeAt(0) - 32 == keyCode) {
-		n.update(true);
+	if (fallingWords[0].word.charCodeAt(0) - 32 == keyCode) {
+		fallingWords[0].update(true);
+		if (fallingWords[0].length() == 0) fallingWords.shift();
 	}
 }
-function addWords(wordsArray) {
-	wordsArray.push(new Word("jesse"));
+function mousePressed() {
+	console.log(hamletText);
 }
