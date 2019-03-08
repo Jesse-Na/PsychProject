be0c1abd5098177142651f41ccaf00c3b0950b82
SodiumNA <jessena.2001@gmail.com>
2019-03-08 15:20:01 -0500

    Added timer,and displays average reaction time at the end of the game, some interface improvements could be made

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -2,16 +2,17 @@ var start = false;
 var beginning = true;
 var fs = false; //controls fullscreen
 var targets = [];
-var time; //game time
-var initTime; //game time of previous frame
+var time1; //overall game timer
+var time2; //reaction timer
+var initTime; //start time
+var prevTime; //game time of the previous frame
 var tbc = []; //time between circles
 var r;
 
 function setup() {
 	let cnv = createCanvas(displayWidth, displayHeight);
 	cnv.style('display', 'block');
-    //initTime = millis();
-    //time = millis();
+    prevTime = millis();
     for (let i = 0; i < 16; i++) {
   		for (let j = 0; j < 3; j++) {
             targets[i] = new Target(displayWidth/3+j*(displayWidth/6), displayHeight/5);
@@ -33,23 +34,21 @@ function setup() {
 }
 
 function draw() {
+  stroke(0);
+  textAlign(CENTER);
 	if (windowWidth != displayWidth || windowHeight != displayHeight){
-	    noFill();
-	    textAlign(CENTER);
-	    stroke(0);
-        background(255);
-		text("Press 'Enter' to start", windowWidth/2, windowHeight/2);
-        start = false;
+    reset();
 	} else {
 		start = true;
 	}
-  	if (start) {
-//        time = millis() - initTime;
-        if (beginning) {
-            visionTest();
-            beginning = false;
-        }
-  	}
+	if (start) {
+    time1 = millis() - initTime;
+    displayTime(time1);
+    time2 = millis()  - prevTime;
+    if (beginning) {
+        visionTest(time2);
+    }
+	} 
 }
 
 function mousePressed() {
@@ -57,35 +56,78 @@ function mousePressed() {
         for (let i = 0; i < 16; i++) {
             if (targets[r].hitbox(mouseX, mouseY)) {
                 visionTest();
-                //initTime = millis();
-                //append(tbc, time);         
+                append(tbc, time2);         
             }        
         }
 	}
 }
 
 function keyPressed() {
-    if (!fs) {
-        if (keyCode === ENTER) {
-            fullscreen(1);
-            fs = true;        
-        }  
-    } else if (fs) {
-        if (keyCode === ENTER) {
-            fullscreen(0);
-            fs = false;        
-        }
-    }
+  if (!fs) {
+      if (keyCode === ENTER) {
+          fullscreen(1);
+          initTime = millis();
+          fs = true;        
+      }  
+  } else if (fs) {
+      if (keyCode === ENTER) {
+          fullscreen(0);
+          tbc = [];
+          fs = false;     
+      }
+  }
 }
 
 function visionTest() {
-	background(255);
-  	strokeWeight(5);
-    r = int(random(16));
-    for (let i = 0; i < 16; i++) {
-        if (i !== r) {
-            targets[i].display();
-        }
-    }
-    targets[r].lightUp();
+  beginning = false;
+  prevTime = millis();
+  background(255);
+  strokeWeight(5);
+  r = int(random(16));
+  for (let i = 0; i < 16; i++) {
+      if (i !== r) {
+          targets[i].display();
+      }
+  }
+  targets[r].lightUp();
+}
+
+function reset() {
+  noFill();
+  background(255);
+  strokeWeight(1);
+  textSize(10);
+  text("Press 'Enter' to start", windowWidth/2, windowHeight/2);
+  start = false;
+  beginning = true;
+}
+
+function displayTime(t) {
+  let time = t;
+  if (time >= 10000) {
+    gameOver();
+  } else {
+    fill(255);
+    noStroke();
+    rect(windowWidth-150, 0, 150, 100);
+    textSize(30);
+    stroke(0);
+    strokeWeight(1);
+    fill(0);
+    text("Time: " + (10-Math.floor(time/1000)), windowWidth-75, 50);
+  }
+}
+
+function gameOver() {
+  var avgTime;
+  var total = 0;
+  for (let i = 0; i < tbc.length; i++) {
+    total = total + tbc[i];
+    avgTime = total/tbc.length;
+  }
+  background(255);
+  fill(0);
+  strokeWeight(1);
+  textSize(10);
+  text(int(Math.floor(avgTime)) + "ms", windowWidth/2, windowHeight/2);
 }
